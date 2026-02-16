ad9674e9e5cf4c24305f5983b7f7564ab796a7df
Rotten120 <thenanokid@gmail.com>
2026-02-16 21:43:17 +0800

    feat: group chats

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { initSocket } from "./socket.js"
+import { prisma } from "./prismaClient.js"
 
 import { createServer } from "node:http"
 import { fileURLToPath } from "node:url"
@@ -11,8 +12,27 @@ const io = initSocket(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/c/:conversationId", async (req, res) => {
+  const { conversationId } = req.params;
+  const conversation = await prisma.conversation.findUnique({
+    where: { id: conversationId }
+  });
+
+  if(!conversation) {
+    return res.sendStatus(404);
+  }
+  
   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.post("/:convo_name", async (req, res) => {
+  const params = req.params;
+  const conversation = await prisma.conversation.create({
+    data: { name: params.convo_name }
+  });
+  res.send(conversation);
 });
 
 const PORT = process.env.PORT;
--- src/socket.js
+++ src/socket.js
@@ -5,45 +5,58 @@ export function initSocket(server) {
   const io = new Server(server);
 
   io.on("connection", async (socket) => {
-        
+    const conversationId = socket.handshake.auth.conversationId;
+
+    console.log(`Connected to room ${conversationId}`);
+
     socket.on('chat message', async (msg, callback) => {
       const message = await prisma.message.create({
-        data: { content: msg }
+        data: { content: msg, conversationId }
       });
 
-      console.log("Message: ", message.content, " ", message.createdAt);
-      io.emit('chat message', message.content, message.createdAt);
+      console.log(`rendered to room  ${conversationId}: `, message.content, " : date: ", message.createdAt);
+      io.to(conversationId).emit('chat message', message.content, message.createdAt);
       callback({ status: 'ok' });
     });
+
+    // message recovery (yes its complicated)
 
     try {
 
       const lastSeen = new Date(socket.handshake.auth.lastMsg);
 
       // the lastSeen condition should be changed
+      // should only execute when first time rendering of client
+      // currently, the server sends all prev messages when connecting
       if(lastSeen == new Date(2000, 10, 30)) {
         // renders the last 10 msgs user have seen
         const prev_messages = await prisma.message.findMany({
-          where: { createdAt: { lte: lastSeen } },
-          orderBy: { createdAt: "desc" },
-          take: 10
+          where: {
+            createdAt: { lte: lastSeen },
+            conversationId
+          },
+          orderBy: { createdAt: "desc" }
         });
       
         if(prev_messages.length > 0) {
           for(let pmsg of prev_messages.reverse()) {
-            console.log('rendered: ', pmsg.content, " : date: ", pmsg.createdAt);
+            console.log(`rendered to room  ${conversationId}: `, pmsg.content, " : date: ", pmsg.createdAt);
             socket.emit('chat message', pmsg.content, pmsg.createdAt)
           }
         }
       }
+
       // renders the messages sent while user is offline
       const missed_messages = await prisma.message.findMany({
-        where: { createdAt: { gt: lastSeen } },
+        where: {
+          createdAt: { gt: lastSeen },
+          conversationId
+        },
         orderBy: { createdAt: "asc" }
       });
 
       for(let msg of missed_messages) {
-        console.log('rendered: ', msg.content, " : date: ", msg.createdAt);
+        console.log(`rendered to room  ${conversationId}: `, msg.content, " : date: ", msg.createdAt);
         socket.emit('chat message', msg.content, msg.createdAt);
       }
 
@@ -51,6 +64,7 @@ export function initSocket(server) {
       console.log("Something went wrong: ", error);
     }
 
+    socket.join(conversationId);
   });
 
   return io;
