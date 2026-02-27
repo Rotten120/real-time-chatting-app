b0bb66b8bbe2885a97aa64f2901a34db39788a24
Rotten120 <thenanokid@gmail.com>
2026-02-27 15:55:38 +0800

    feat: username change route

--- src/api/userRoutes.js
+++ src/api/userRoutes.js
@@ -18,29 +18,34 @@ router.get("/chatrooms", async (req, res) => {
   res.json(chatRooms);
 });
 
+router.patch("/name", async (req, res) => {
+  const { newName } = req.body;
+  
+  if(!newName) {
+    return res.status(400).send({ message: "New Name is a required field" });
+  }
+
+  await prisma.user.update({
+    data: { name: newName },
+    where: { id: req.user.id }
+  });
+
+  res.send({ message: "Name has been successfully changed" });
+});
+
 // this does not have input validation (i.e. password length etc.)
 // could be validated over the client
-router.post("/password", async (req, res) => {
+router.patch("/password", async (req, res) => {
   const { newPassword, oldPassword } = req.body;
+
+  if(!oldPassword || !newPassword) {
+    return res.status(400).json({ message: "New password and Old password are required" })
+  }
 
   const user = await prisma.user.findUnique({
     where: { id: req.userId },
     select: { password: true }
   });
-
-  if(!user.password) {
-    const hashedPassword = await hashPassword(newPassword);
-    await prisma.user.update({
-      where: { id: req.userId },
-      data: { password: hashedPassword }
-    });
-
-    return res.json({ message: "Password set successfully" })
-  }
-
-  if(!oldPassword) {
-    return res.status(400).json({ message: "Old password is required" })
-  }
 
   const isVerified = verifyPassword(oldPassword, user.password);
   if(!isVerified) {
